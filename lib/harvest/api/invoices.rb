a5eb98c99540cb8858458f7d1282ac894e81bbe7
Brian <bcobb@uwalumni.com>
2011-09-25 20:23:39 -0500

    Add outstanding_at_page method to invoice API

--- lib/harvest/api/invoices.rb
+++ lib/harvest/api/invoices.rb
@@ -4,6 +4,13 @@ module Harvest
       api_model Harvest::Invoice
       include Harvest::Behavior::Crud
       
+      def outstanding_at_page(page = 1, user = nil)
+        path = api_model.api_path + '?status=draft&page=' + page.to_s
+        response = request(:get, credentials, path, :query => of_user_query(user))
+
+        return api_model.parse(response.parsed_response)
+      end
+
       def create(*)
         raise "Creating and updating invoices are not implemented due to API issues"
       end
